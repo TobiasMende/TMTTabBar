c8de2ecfecb889216fdab106132320927f6f1baa
Tobias Mende <tobias.mende@tobsolution.de>
2015-10-26 07:04:06 +0100

    add borders and backgrounds

--- TMTTabBar/TMTTabView.m
+++ TMTTabBar/TMTTabView.m
@@ -8,12 +8,45 @@
 
 #import "TMTTabView.h"
 
+@interface TMTTabView ()
+- (void)drawBorders;
+
+- (void)drawBackground;
+
+- (void)drawLabel;
+@end
+
 @implementation TMTTabView
 
 - (void)drawRect:(NSRect)dirtyRect {
     [super drawRect:dirtyRect];
-    [self.label drawInRect:self.bounds withAttributes:nil];
-    // Drawing code here.
+
+    [self drawBackground];
+    [self drawBorders];
+    [self drawLabel];
+}
+
+- (void)drawBorders {
+    [[NSColor controlShadowColor] setStroke];
+
+    CGPoint upperLeft = self.bounds.origin;
+    CGPoint upperRight = CGPointMake(upperLeft.x + self.bounds.size.width, upperLeft.y);
+    [self drawVerticalBorderForUpper:upperRight];
+}
+
+- (void)drawVerticalBorderForUpper:(CGPoint)upper {
+    CGPoint lower = CGPointMake(upper.x, upper.y  + self.bounds.size.height);
+    [NSBezierPath strokeLineFromPoint:upper toPoint:lower];
+}
+
+- (void)drawBackground {
+    [[NSColor controlBackgroundColor] setFill];
+    NSRectFill(self.bounds);
+}
+
+- (void)drawLabel {
+    NSDictionary *attributes = @{NSForegroundColorAttributeName : [NSColor secondaryLabelColor]};
+    [self.label drawInRect:self.bounds withAttributes:attributes];
 }
 
 @end
